66bf0e98847d575f2e839f55ec3bd98c59c15b32
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-07-04 15:39:46 +0200

    Fixed typo

--- cmake/debug.cmake
+++ cmake/debug.cmake
@@ -1,6 +1,6 @@
 add_custom_target("debug"
     COMMENT "Debugging with GDB"
-    DEPENDS ${PROJECT_TEST_NAME}
+    DEPENDS ${PROJECT_BIN}
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND gdb -tui bin/${PROJECT_TEST_NAME}
+    COMMAND gdb -tui bin/${PROJECT_BIN}
 )
